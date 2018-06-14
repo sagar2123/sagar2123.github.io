72872817537f9506e14f6e6c7bda831591fa41ee
TRICONHOME\sagarb <sagarb@triconinfotech.com>
2018-06-14 19:50:42 +0530

    products added

--- react-mobx/src/App.js
+++ react-mobx/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {observer} from 'mobx-react';
 
+@observer
 class App extends Component {
+
   render() {
+      console.log(this.props.store.products)
+    var productsList = this.props.store.products.map(function(obj) {
+        // obj.img = this.convertImage();
+        return <div key={obj.id} className="col-md-4 col-lg-4 col-sm-4">  
+            <div className="product-card">
+              <img src={obj.img} className="product-img" alt=""/>
+              <h6>{obj.name}</h6>
+            </div>
+        </div>
+      }
+    )
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 col-lg-12 col-sm-12">
+                    <h1 className="text-center heading">Select any two products you want to compare</h1>
+            </div>
+          </div> 
+            <div className="row">
+              {productsList}
+          </div>  
+        </div>
       </div>
     );
   }
--- react-mobx/src/index.js
+++ react-mobx/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import ProductStore from './store/productStore';
+const store = new ProductStore();
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
